fa905d3b76da233343c8896123e30028f35718b5
isman7 <isman7@gmail.com>
2016-06-18 10:37:32 +0200

    Hand made request to the Instagram API in order to know the number of followers.

--- telegram-bot.py
+++ telegram-bot.py
@@ -12,19 +12,16 @@ import telepot
 import random
 import datetime
 
+#"Handmade" API
+import json
+import requests
+
 
 
 def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
-
     print('Got command: %s' % command)
-
-#    if command == '/roll':
-#        bot.sendMessage(chat_id, random.randint(1,6))
-#    elif command == '/time':
-#        bot.sendMessage(chat_id, str(datetime.datetime.now()))
-
     command = command.split('@')[0]
     
     if command[0] == '/':
@@ -32,6 +29,14 @@ def handle(msg):
             bot.sendMessage(chat_id, 'Hola! Sóc l\'Insta Gramsci Bot un robot creat per l\'Instagram de la JCC.')
         elif command == '/help':
             bot.sendMessage(chat_id, 'Hola! Sóc l\'Insta Gramsci Bot un robot creat per l\'Instagram de la JCC.')
+        elif command == '/followers':
+            response = requests.get(url)
+            j = json.loads(response.text)
+            followers = str(j['data']['counts']['followed_by'])
+            bot.sendMessage(chat_id, \
+            'El compte [@joventutcomunistacat](https://www.instagram.com/joventutcomunistacat/) té actualment *%s* persones com a seguidores!'\
+            % followers, parse_mode='Markdown')
+            
         else:
             bot.sendMessage(chat_id, \
             'Disculpes, sembla que la comanda %s no ha sigut implementada pel soviet.'\
@@ -41,9 +46,15 @@ def handle(msg):
         'Disculpes però sembla que \'%s\' és un significant massa buit per mi. Utilitza comandes de Telegram com /help'\
         % command)
 
-TOKEN = sys.argv[1]  # get token from command-line
+telegram_token = sys.argv[1]  # get token from command-line
+insta_access_token = sys.argv[2]
+insta_client_id = sys.argv[3]
+insta_client_secret = sys.argv[4]
 
-bot = telepot.Bot(TOKEN)
+url = "https://api.instagram.com/v1/users/self/?access_token={0}".format(insta_access_token)
+
+
+bot = telepot.Bot(telegram_token)
 bot.message_loop(handle)
 print('Listening ...')
 
